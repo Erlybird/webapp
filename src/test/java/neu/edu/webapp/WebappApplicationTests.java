a76aa8df0678078fa311a231b21d09b93f589100
Sangram Vuppala <92066602+Erlybird@users.noreply.github.com>
2023-10-13 17:14:47 -0400

    Corrections after review (#16)
    * Made changes to Post, put and delte methods
    
    * updated Test
    
    * Delete request updated
    
    * Change in test
    
    * small changes

--- src/test/java/neu/edu/webapp/WebappApplicationTests.java
+++ src/test/java/neu/edu/webapp/WebappApplicationTests.java
@@ -8,23 +8,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebappApplicationTests {
 
-    @SpringBootTest
-    @AutoConfigureMockMvc
-    class AssignmentCloud1ApplicationTests {
-
-        //        @Autowired
+//        @Autowired
 //        private MockMvc mv;
 //
 //        @MockBean
@@ -39,15 +40,35 @@ class WebappApplicationTests {
 //                    .contentType(MediaType.APPLICATION_JSON));
 //
 //            // Verify that the response has status 200 OK
+//            if (!connectionCheckService.isDatabaseConnected()) {
+//                result.andExpect(status().isForbidden());
+//            }
 //            result.andExpect(status().isOk());
-        @Autowired
-        ConnectionCheckDB connectionCheckDB;
+//        @Autowired
+//        ConnectionCheckDB connectionCheckDB;
+//
+//        @Test
+//        public void testDatabase(){
+//            assert connectionCheckDB.checkDBConnection();
+//    }
 
-        @Test
-        public void testDatabase(){
-            assert connectionCheckDB.checkDBConnection();
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testDatabaseConnection() {
+        String url = "http://localhost:" + port + "/healthz";
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+        // Assert that the response status code is 200 OK
+        assertEquals(200, responseEntity.getStatusCodeValue());
+
+
+
     }
-
 }
 
 }
