d900156ec48a76a7f3ed9560951a2d536075f216
Sangram Vuppala <92066602+Erlybird@users.noreply.github.com>
2023-12-02 21:07:10 -0500

    Removed mysql from AMI

--- Java.sh
+++ Java.sh
@@ -6,8 +6,8 @@ sudo apt-get install unzip
 
 
 #install jdk
-echo "---x---Adding Java Repository---x---"
-sudo apt update && sudo apt upgrade -y
+#echo "---x---Adding Java Repository---x---"
+#sudo apt update && sudo apt upgrade -y
 
 echo "---x---Installing Java 17---x---"
 sudo apt install openjdk-17-jdk -y
@@ -22,27 +22,27 @@ echo "---x---Installing Maven---x---"
 sudo apt install maven -y
 
 #Install MariaDb
-#sudo apt update
-echo "sudo apt install mariadb-server"
-sudo apt install -y mariadb-server
-
-sudo systemctl start mariadb
-echo "sudo systemctl enable mariadb"
-sudo systemctl enable mariadb
-
-echo "sudo mysql_secure_installation"
-sudo mysql_secure_installation <<EOF
-Ssangramm@12
-n
-y
-Ssangramm@12
-Ssangramm@12
-n
-n
-n
-y
-EOF
-
+##sudo apt update
+#echo "sudo apt install mariadb-server"
+#sudo apt install -y mariadb-server
+#
+#sudo systemctl start mariadb
+#echo "sudo systemctl enable mariadb"
+#sudo systemctl enable mariadb
+#
+#echo "sudo mysql_secure_installation"
+#sudo mysql_secure_installation <<EOF
+#Ssangramm@12
+#n
+#y
+#Ssangramm@12
+#Ssangramm@12
+#n
+#n
+#n
+#y
+#EOF
+#
 
 sudo apt update
 sudo apt upgrade -y
